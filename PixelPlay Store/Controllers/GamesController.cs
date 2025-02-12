3b2a43df2cee61dc986d54778dbd4b753610c2f1
Mohamed7oksh23 <mohamed.osama2313@gmail.com>
2025-02-12 21:36:40 +0200

    Add Game Controller and it's Views for CRUD operation

---
+++ PixelPlay Store/Controllers/GamesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PixelPlay.Controllers
+{
+    public class GamesController : Controller
+    {
+        public GamesController() 
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View("Details", id);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        public IActionResult Create(Games games)
+        {
+            return RedirectToAction();
+        }
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View("Update");
+        }
+        [HttpPost]
+        public IActionResult Update(Games games)
+        {
+            return RedirectToAction();
+        }
+
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            return View("Delete");
+        }
+        [HttpPost]
+        public IActionResult Delete(Games games)
+        {
+            return RedirectToAction();
+        }
+
+
+    }
+}
--- PixelPlay Store/Views/Globals.cs
+++ PixelPlay Store/Views/Globals.cs
@@ -1,3 +1,4 @@
-﻿global using PixelPlay.Data;
+﻿global using PixelPlay.Models;
+global using PixelPlay.Data;
 global using Microsoft.EntityFrameworkCore;
 global using System.ComponentModel.DataAnnotations;
