68aed4f19b4ec4e78a5f1fcb6101e24ab2505d35
Mohamed7oksh23 <mohamed.osama2313@gmail.com>
2025-06-21 01:23:53 +0300

    Make Home Page Dynamically Showing Categories & Associated Games

--- PixelPlay Store/Controllers/HomeController.cs
+++ PixelPlay Store/Controllers/HomeController.cs
@@ -18,83 +18,13 @@ namespace PixelPlay_Store.Controllers
             categoriesrepo = categoriesRepo;
         }
 
+
         [HttpGet]
         public async Task<IActionResult> Index()
-        {
-            var games = await gamesrepo.GetAll().Take(8).ToListAsync();
+        {           
+            var games = await gamesrepo.GetAll().ToListAsync();
 
-            var game = await gamesrepo.GetAll().ToListAsync();
-
-            var categories = await categoriesrepo.GetCategory().ToListAsync();
-
-            //Get all Action Games separately
-            var actionGames = game
-                .Where(g => g.GameCategories.Any(gc => gc.CategoryId == 1))
-                .Take(6).ToList();
-
-            //Get all Adventure Games separately
-            var adventureGames = game
-                .Where(g => g.GameCategories.Any(gc => gc.CategoryId == 2))
-                .Take(6).ToList();
-
-            //Get all Arcade Games separately
-            var arcadeGames = game
-                .Where(g => g.GameCategories.Any(gc => gc.CategoryId == 3))
-                .Take(6).ToList();
-
-            //Get all Horror Games separately
-            var horrorGames = game
-                .Where(g => g.GameCategories.Any(gc => gc.CategoryId == 4))
-                .Take(6).ToList();
-
-            //Get all Fighting Games separately
-            var fightingGames = game
-                .Where(g => g.GameCategories.Any(gc => gc.CategoryId == 5))
-                .Take(6).ToList();
-
-            //Get all Story Games separately
-            var storyGames = game
-                .Where(g => g.GameCategories.Any(gc => gc.CategoryId == 6))
-                .Take(6).ToList();
-
-            //Get all Shooting Games separately
-            var shootingGames = game
-                .Where(g => g.GameCategories.Any(gc => gc.CategoryId == 7))
-                .Take(6).ToList();
-
-            //Get all Sport Games separately
-            var sportGames = game
-                .Where(g => g.GameCategories.Any(gc => gc.CategoryId == 8))
-                .Take(6).ToList();
-
-            //Get all Survival Games separately
-            var survivalGames = game
-                .Where(g => g.GameCategories.Any(gc => gc.CategoryId == 9))
-                .Take(6).ToList();
-
-            //Get all Drama Games separately
-            var dramaGames = game
-                .Where(g => g.GameCategories.Any(gc => gc.CategoryId == 10))
-                .Take(6).ToList();
-
-            //Create a view model
-            var viewModel = new GamesIndexViewModel
-            {
-                PagedGames = games,
-                ActionGames = actionGames,
-                AdventureGames = adventureGames,
-                ArcadeGames = arcadeGames,
-                HorrorGames = horrorGames,
-                FightingGames = fightingGames,
-                StoryGames = storyGames,
-                ShootingGames = shootingGames,
-                SportGames = sportGames,
-                SurvivalGames = survivalGames,
-                DramaGames = dramaGames,
-                Categories = categories             //*************
-            };
-
-            return View(viewModel);
+            return View(games);
         }
 
 
